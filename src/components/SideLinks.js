d258a2d0fda8d6187fab3bbf562baaa33fb38e38
Swhag <khg5293@outlook.com>
2023-01-29 13:28:31 -0500

    side links UI updated

--- src/components/SideLinks.js
+++ src/components/SideLinks.js
@@ -1,25 +1,39 @@
 import React from 'react';
-
 import '../styles/sideLinks.css';
 
 function SideLinks(props) {
   return (
     <div className='side-links-container'>
       <ul>
-        <li className='side-links'>
-          <a href='https://www.linkedin.com/in/caleb-kim-76a440209'>LinkedIn</a>
-
-          <i className='fa-brands fa-linkedin'></i>
+        <li className='side-links linkedin'>
+          <a
+            href='https://www.linkedin.com/in/caleb-kim-76a440209'
+            target='_blank'
+          >
+            <span>LinkedIn</span>
+            <i className='fa-brands fa-linkedin'></i>
+          </a>
         </li>
 
-        <li className='side-links'>
-          <p>GitHub</p>
-          <i className='fa-brands fa-github'></i>
+        <li className='side-links github'>
+          <a href='https://github.com/Swhag' target='_blank'>
+            <span>GitHub</span>
+            <i className='fa-brands fa-github'></i>
+          </a>
         </li>
 
-        <li className='side-links'>
-          <p>Resume</p>
-          <i className='fa-regular fa-file-lines'></i>
+        <li className='side-links resume'>
+          <a href='https://github.com/Swhag' target='_blank'>
+            <span>Resume</span>
+            <i className='fa-regular fa-file-lines'></i>
+          </a>
+        </li>
+
+        <li className='side-links email'>
+          <a href='https://github.com/Swhag' target='_blank'>
+            <span>Email</span>
+            <i className='fa-regular fa-envelope'></i>
+          </a>
         </li>
       </ul>
     </div>
