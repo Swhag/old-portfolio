6064deb1a50e0939d230e71028f6338ede78d874
Swhag <khg5293@outlook.com>
2023-01-31 01:05:56 -0500

    project left updated (video layout placed in the left position)

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -51,10 +51,60 @@ function Projects(props) {
             </div>
           </div>
 
-          <div className='video-overlay-right'>
-            <video className='video1' controls muted preload='metadata'>
+          <div className='video-overlay'>
+            <video className='video-right' controls muted preload='metadata'>
               <source src='../Videos/e-commerce.mp4' type='video/mp4' />
             </video>
+          </div>
+        </div>
+
+        <div className='project-left'>
+          <div className='video-overlay'>
+            <video className='video-left' controls muted preload='metadata'>
+              <source src='../Videos/CVBuilder.mp4' type='video/mp4' />
+            </video>
+          </div>
+
+          <div className='project-info'>
+            <div className='project-description'>
+              <h3>Resumer Builder Web App</h3>
+              <p>
+                Resume-Builder Web Application with minimalist design. Built
+                using ReactJS, React-to-print, UUID library and CSS.
+              </p>
+              <ul>
+                <h4>Features</h4>
+                <li>
+                  Real-time rendering of Resume Input and Output using State
+                  management
+                </li>
+                <li>Generate PDF (targets CV preview component only)</li>
+                <li>
+                  Clean user interface with smooth animations (vanilla CSS)
+                </li>
+                <li>Responsive design (mobile friendly UI)</li>
+              </ul>
+            </div>
+
+            <div className='button-group'>
+              <div className='btn'>
+                <a
+                  href='https://swhag.github.io/Resume-Builder-App/'
+                  target='_blank'
+                >
+                  <span>Live App</span>
+                </a>
+              </div>
+              <div className='btn'>
+                <a
+                  href='https://github.com/Swhag/Resume-Builder-App'
+                  target='_blank'
+                >
+                  <i className='fa-solid fa-code'></i>
+                  <span>Code</span>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
