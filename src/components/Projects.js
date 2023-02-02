588c2dd5de2bfeb37be44f3d5761b7677f4603f9
Swhag <khg5293@outlook.com>
2023-02-02 01:43:11 -0500

    project re-render depending on window.innerWidth value

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/projects.css';
 
 function Projects(props) {
   const { projects } = props;
+  const [mobile, setMobile] = useState(null);
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function checkWidth() {
+      setWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', checkWidth);
+
+    return () => {
+      window.removeEventListener('resize', checkWidth);
+    };
+  });
+
+  useEffect(() => {
+    if (width <= 1024) {
+      setMobile(true);
+    } else {
+      setMobile(false);
+    }
+  }, [width]);
 
   return (
     <div className='projects-section' ref={projects}>
@@ -13,15 +35,31 @@ function Projects(props) {
       </div>
 
       <div className='projects-container'>
-        <Project1></Project1>
-        <Project2></Project2>
-        <Project3></Project3>
+        <Project1 mobile={mobile}></Project1>
+        <Project2 mobile={mobile}></Project2>
+        <Project3 mobile={mobile}></Project3>
       </div>
     </div>
   );
 }
 
-function Project1() {
+function Project1(props) {
+  const { mobile } = props;
+  const video1 = (
+    <div className='video-overlay'>
+      <video
+        className='video-right'
+        controls
+        autoPlay
+        muted
+        loop
+        preload='metadata'
+      >
+        <source src='../Videos/e-commerce.mp4' type='video/mp4' />
+      </video>
+    </div>
+  );
+
   return (
     <div className='project-right'>
       <div className='project-info'>
@@ -31,6 +69,9 @@ function Project1() {
             Simple E-Commerce Web Application built with ReactJS, Redux, Axios,
             React Router and Bootstrap to strap everything together.
           </p>
+
+          {mobile === true ? video1 : null}
+
           <ul>
             <h4>Features</h4>
             <li>Clean user interface with smooth animations</li>
@@ -38,46 +79,53 @@ function Project1() {
             <li>Filter products by categories</li>
             <li>Responsive design</li>
           </ul>
-        </div>
 
-        <div className='button-group'>
-          <div className='btn'>
-            <a
-              href='https://swhag.github.io/React-E-Commerce-App/'
-              target='_blank'
-            >
-              <span>Live App</span>
-            </a>
-          </div>
-          <div className='btn'>
-            <a
-              href='https://github.com/Swhag/React-E-Commerce-App'
-              target='_blank'
-            >
-              <span>Code</span>
-              <i className='fa-solid fa-code'></i>
-            </a>
+          <div className='button-group'>
+            <div className='btn'>
+              <a
+                href='https://swhag.github.io/React-E-Commerce-App/'
+                target='_blank'
+              >
+                <span>Live App</span>
+              </a>
+            </div>
+            <div className='btn'>
+              <a
+                href='https://github.com/Swhag/React-E-Commerce-App'
+                target='_blank'
+              >
+                <span>Code</span>
+                <i className='fa-solid fa-code'></i>
+              </a>
+            </div>
           </div>
         </div>
       </div>
-
-      <div className='video-overlay'>
-        <video className='video-right' controls muted preload='metadata'>
-          <source src='../Videos/e-commerce.mp4' type='video/mp4' />
-        </video>
-      </div>
+      {mobile === false ? video1 : null}
     </div>
   );
 }
 
-function Project2() {
+function Project2(props) {
+  const { mobile } = props;
+  const video2 = (
+    <div className='video-overlay'>
+      <video
+        className='video-left'
+        controls
+        autoPlay
+        muted
+        loop
+        preload='metadata'
+      >
+        <source src='../Videos/CVBuilder.mp4' type='video/mp4' />
+      </video>
+    </div>
+  );
+
   return (
     <div className='project-left'>
-      <div className='video-overlay'>
-        <video className='video-left' controls muted preload='metadata'>
-          <source src='../Videos/CVBuilder.mp4' type='video/mp4' />
-        </video>
-      </div>
+      {mobile === false ? video2 : null}
 
       <div className='project-info'>
         <div className='project-description'>
@@ -86,6 +134,9 @@ function Project2() {
             Resume-Builder Web Application with minimalist design. Built using
             ReactJS, React-to-print, UUID library and CSS.
           </p>
+
+          {mobile === true ? video2 : null}
+
           <ul>
             <h4>Features</h4>
             <li>
@@ -96,25 +147,25 @@ function Project2() {
             <li>Clean user interface with smooth animations (vanilla CSS)</li>
             <li>Responsive design (mobile friendly UI)</li>
           </ul>
-        </div>
 
-        <div className='button-group'>
-          <div className='btn'>
-            <a
-              href='https://swhag.github.io/Resume-Builder-App/'
-              target='_blank'
-            >
-              <span>Live App</span>
-            </a>
-          </div>
-          <div className='btn'>
-            <a
-              href='https://github.com/Swhag/Resume-Builder-App'
-              target='_blank'
-            >
-              <span>Code</span>
-              <i className='fa-solid fa-code'></i>
-            </a>
+          <div className='button-group'>
+            <div className='btn'>
+              <a
+                href='https://swhag.github.io/Resume-Builder-App/'
+                target='_blank'
+              >
+                <span>Live App</span>
+              </a>
+            </div>
+            <div className='btn'>
+              <a
+                href='https://github.com/Swhag/Resume-Builder-App'
+                target='_blank'
+              >
+                <span>Code</span>
+                <i className='fa-solid fa-code'></i>
+              </a>
+            </div>
           </div>
         </div>
       </div>
@@ -122,7 +173,23 @@ function Project2() {
   );
 }
 
-function Project3() {
+function Project3(props) {
+  const { mobile } = props;
+  const video3 = (
+    <div className='video-overlay'>
+      <video
+        className='video-right'
+        controls
+        autoPlay
+        muted
+        loop
+        preload='metadata'
+      >
+        <source src='../Videos/WeatherApp.mp4' type='video/mp4' />
+      </video>
+    </div>
+  );
+
   return (
     <div className='project-right'>
       <div className='project-info'>
@@ -133,6 +200,9 @@ function Project3() {
             JavaScript Async/Await function, API fetch calls and DOM
             manipulation.
           </p>
+
+          {mobile === true ? video3 : null}
+
           <ul>
             <h4>Features</h4>
             <li>
@@ -144,28 +214,23 @@ function Project3() {
               RRender appropriate weather icons for each forecast description
             </li>
           </ul>
-        </div>
 
-        <div className='button-group'>
-          <div className='btn'>
-            <a href='https://swhag.github.io/Weather-App/' target='_blank'>
-              <span>Live App</span>
-            </a>
-          </div>
-          <div className='btn'>
-            <a href='https://github.com/Swhag/Weather-App' target='_blank'>
-              <span>Code</span>
-              <i className='fa-solid fa-code'></i>
-            </a>
+          <div className='button-group'>
+            <div className='btn'>
+              <a href='https://swhag.github.io/Weather-App/' target='_blank'>
+                <span>Live App</span>
+              </a>
+            </div>
+            <div className='btn'>
+              <a href='https://github.com/Swhag/Weather-App' target='_blank'>
+                <span>Code</span>
+                <i className='fa-solid fa-code'></i>
+              </a>
+            </div>
           </div>
         </div>
       </div>
-
-      <div className='video-overlay'>
-        <video className='video-right' controls muted preload='metadata'>
-          <source src='../Videos/WeatherApp.mp4' type='video/mp4' />
-        </video>
-      </div>
+      {mobile === false ? video3 : null}
     </div>
   );
 }
